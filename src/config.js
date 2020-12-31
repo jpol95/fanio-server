ae10d8a6e4c14871a6667b7680122a9ae2e77e68
Jesse Pollack <jesse.lincolndouglas@gmail.com>
2020-12-31 03:23:36 -0500

    trying to deploy

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || "postgresql://dundermiffling:1Pepperm%40n@localhost/fanio",
-    TEST_DATABASE_URL: process.env.DATABASE_TEST_URL || "postgresql://dundermiffling:1Pepperm%40n@localhost/fanio-test", 
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://dundermiffling:1Pepperm%40n@localhost/fanio-test", 
     JWT_SECRET: process.env.JWT_SECRET || "zutara", 
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://fanio-client.vercel.app'
   }
